b76abcf96373462a682a0e882170c20f0def2b4f
ShristiA <shristiamatya3@gmail.com>
2018-10-11 10:21:38 -0400

    answered

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,16 +3,20 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-	ArrayList<String> al = new ArrayList<>();
-	al.add("module");
-	al.add("class");
-	al.add("implement");
+
+	ArrayList<String> items = new ArrayList<>();
+
+	items.add("module");
+	items.add("class");
+	items.add("implement");
+
 	Random r = new Random();
-	for(int i =0; i<3; i++) {
-        int x = r.nextInt(3);
-        al.listIterator(x);
-        System.out.println(al);
-    }
+	int itemIndex = r.nextInt(items.size());
+	// we are getting random index from the array list by using random class.
+	String item = items.get(itemIndex);
+	//getting value at the random index.
+
+	System.out.println(item);
 
     }
 }
